2dede1072bcb2a148df9add7cb3d046a028faa0e
Richard-vinu <richardnova1999@gmail.com>
2022-04-26 18:02:41 +0530

    added authorSchema

---
+++ src/models/authorModel.js
@@ -0,0 +1,53 @@
+const { default: mongoose } = require("mongoose")
+
+const authorSchema = new mongoose.Schema(
+{ 
+    fname: {
+        type: String,
+        required :true,
+       }, 
+lname: {type: String,
+    required: true,
+}, 
+title: {type: String, 
+    enum:["Mr", "Mrs"," Miss"]
+},
+ email: {type:String,
+    required:true ,
+    
+    validate: { validator: function(v) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+    unique:true
+    }
+},
+     password: {type:String,
+    required:true} 
+ },{timestamps:true});
+
+module.exports=mongoose.model('author',authorSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// { fname: { mandatory}, lname: {mandatory},
+//  title: {mandatory, enum[Mr, Mrs, Miss]}, 
+//  email: {mandatory, valid email, unique}, password: {mandatory} }
+// validate: [validateEmail, 'Please fill a valid email address'],
+// match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
