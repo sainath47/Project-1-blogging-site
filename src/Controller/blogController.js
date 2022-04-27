be5b72ea7348786f6935fbde1e7793871b5992aa
sainath47 <reddysainth47@gmail.com>
2022-04-27 06:12:08 +0530

    slight modification in blog controller code (line18)

--- src/Controller/blogController.js
+++ src/Controller/blogController.js
@@ -11,15 +11,17 @@ const blogModel = require("../models/blogModel");
 const createBlog = async function (req, res) {
 
   try {
+    console.log(req.body)
     if (req.body) {
 
         let authorId = req.body.authorId;
+        if(!authorId) return res.send({msg:"authorId is required"})
         let validationAuthorId = await authorModel.findById(authorId);
         if (!validationAuthorId) return res.send({ msg: "enter valid authorId" });
 
       let blog = req.body;
       let blogCreated = await blogModel.create(blog);
-      console.log(blogCreated);
+      // console.log(blogCreated);
       res.status(201).send({ data: blogCreated });
     } else {
       return res.status(400).send({ msg: "Bad request" });
@@ -32,7 +34,7 @@ const createBlog = async function (req, res) {
 
 const filterBlogs= async function(req,res){
 
-    let data = await blogModel.find({isDeleted:false},{published:true})
+    let data = await blogModel.find($and[{isDeleted:false},{published:true}])
     res.send({msg:data})
 }
 
